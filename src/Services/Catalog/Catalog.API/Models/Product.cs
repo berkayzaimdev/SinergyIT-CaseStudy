cf374e75b3a87f44871e7a42f1dc3cfcf2a2f771
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-11 11:21:41 +0300

    added Product entity

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Models;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    [BsonElement(Order = 0)]
+    public Guid Id { get; set; }
+
+	[BsonRepresentation(MongoDB.Bson.BsonType.String)]
+	[BsonElement(Order = 1)]
+	public string Name { get; set; } = default!;
+
+	[BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+	[BsonElement(Order = 2)]
+	public decimal Price { get; set; }
+}
