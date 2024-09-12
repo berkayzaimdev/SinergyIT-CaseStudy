e073a32f8ed118ffe3e87ccfec58282d9c4e9619
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-12 18:04:23 +0300

    created: Home page

---
+++ src/Client/ecommerce-web-app/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div className="home-container">
+      <h1>Hello, World!</h1>
+      <p>Welcome to the home page of your React application.</p>
+    </div>
+  );
+};
+
+export default Home;
