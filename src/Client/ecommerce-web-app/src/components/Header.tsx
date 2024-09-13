26f60dff97d4aa393535430f1059adef20cafb35
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-13 11:28:06 +0300

    modified: Header.tsx

--- src/Client/ecommerce-web-app/src/components/Header.tsx
+++ src/Client/ecommerce-web-app/src/components/Header.tsx
@@ -7,18 +7,22 @@ const Header: React.FC = () => {
   return (
     <Navbar expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">e-Commerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <Nav.Link href="#home">Products</Nav.Link>
+            <Nav.Link href="#link">Cart</Nav.Link>
+            <NavDropdown title="Brands" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Brands</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
