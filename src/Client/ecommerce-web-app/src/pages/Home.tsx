46a71ed17e42b83302ca4f9e461f9ebf8ab98b32
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-13 10:19:21 +0300

    Added some components

--- src/Client/ecommerce-web-app/src/pages/Home.tsx
+++ src/Client/ecommerce-web-app/src/pages/Home.tsx
@@ -1,8 +1,35 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Header from "../components/Header";
+import ProductCard from "../components/ProductCard";
+
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      <h1>Hello, World!</h1>
-      <p>Welcome to the home page of your React application.</p>
+      <Header></Header>
+      <Container>
+        <Row className="mt-4">
+          <Col>
+            <ProductCard />
+          </Col>
+          <Col>
+            <ProductCard />
+          </Col>
+          <Col>
+            <ProductCard />
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col>
+            <ProductCard />
+          </Col>
+          <Col>
+            <ProductCard />
+          </Col>
+          <Col>
+            <ProductCard />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
