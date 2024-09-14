bab85e9bf6d88a842f706ac98a670628437e6f1d
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-14 21:36:20 +0300

    created: Order service

--- src/Services/Identity/Identity.API/Features/Auth/Register/RegisterEndpoint.cs
+++ src/Services/Identity/Identity.API/Features/Auth/Register/RegisterEndpoint.cs
@@ -1,5 +1,9 @@
 ﻿namespace Identity.API.Features.Auth.Register;
 
-public class RegisterEndpoint
+public class RegisterEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
 }
