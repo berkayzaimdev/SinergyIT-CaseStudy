926cd21753d7c0edbcb9009c2cac87ead10e4f0d
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-15 09:41:24 +0300

    created: Order and OrderItem entities

---
+++ src/Order/Order.Domain/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.Domain.Models;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public ICollection<OrderItem> Items { get; set; } = [];
+}
---
+++ src/Order/Order.Domain/Models/OrderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Order.Domain.Models;
+
+public class OrderItem
+{
+    public Guid OrderId { get; set; }
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+}
