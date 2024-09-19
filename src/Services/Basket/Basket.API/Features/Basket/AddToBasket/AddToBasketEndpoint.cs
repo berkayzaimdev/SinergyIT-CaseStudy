8df3140a3e61950faa7dda8c1ca4aaae9f48e4d6
berkayzaimdev <berkayzaimdev@gmail.com>
2024-09-19 13:53:53 +0300

    modified: AddToBasketEndpoint

--- src/Services/Basket/Basket.API/Features/Basket/AddToBasket/AddToBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Features/Basket/AddToBasket/AddToBasketEndpoint.cs
@@ -1,12 +1,24 @@
-﻿
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Basket.API.Features.Basket.AddToBasket;
 
-public record AddToBasketRequest(string ProductId) : ICommand<AddToBasketResult>;
+//public record AddToBasketRequest(string ProductId);
 public record AddToBasketResponse(bool IsSuccess);
 public class AddToBasketEndpoint : ICarterModule
 {
 	public void AddRoutes(IEndpointRouteBuilder app)
 	{
-		throw new NotImplementedException();
+		app.MapPost("/add-to-basket", [Authorize] (HttpContext context, string productId) =>
+		{
+			var userId = context.User.FindFirst("userId")?.Value;
+
+			if (userId == null)
+			{
+				return Results.BadRequest("User ID not found.");
+			}
+
+			return Results.Ok(new { UserId = userId, ProductId = productId });
+		});
 	}
 }
